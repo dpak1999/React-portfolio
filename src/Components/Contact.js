48bf7573b105504c643826af2a3b449cc9c8bd2f
dpak1999 <deepakkumardash313@gmail.com>
2020-09-20 17:24:58 +0530

    change contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -13,7 +13,9 @@ const Contact = ({ data }) => {
 
   const handleClick = (e) => {
     e.preventDefault();
-    window.open(`mailto:${email}?subject=${subject}&body=${name}: ${message}`);
+    window.open(
+      `mailto:deepakkumardash313@gmail.com?subject=${subject}&body=${name}: ${message}`
+    );
   };
 
   return (
